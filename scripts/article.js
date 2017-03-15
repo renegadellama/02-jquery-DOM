b7ac6345095a0a72aa4f337b2f02f093f3dc04eb
galang pham <phamgalang@gmail.com>
2017-03-14 17:52:46 -0700

    finished all todo

--- scripts/article.js
+++ scripts/article.js
@@ -9,35 +9,21 @@ function Article (opts) {
   this.authorUrl = opts.authorUrl;
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
-  // TODO: Use the JS object passed in to complete this constructor function:
-  // Save ALL the properties of `opts` into `this`
 }
-articles.push(rawData);
-console.log(articles);
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
 
-
-  /* TODO: This cloned article is no longer a template,
-  as it now has real data attached to it! We need to account
-  for that before this current article gets rendered to our
-  DOM. */
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('address a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorURL);
+  $newArticle.find('.article-body').html(this.body);
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
-
-  /* TODO: Now use jQuery to fill in the rest of the current
-  template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
-
   // Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
